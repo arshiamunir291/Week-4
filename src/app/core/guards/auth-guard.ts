68c18ae3498926f362e58ba9a7c57b39943905a3
arshiamunir291 <arshiamunir786@gmail.com>
2026-03-06 05:25:14 -0500

    Update route.ts

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -8,7 +8,7 @@ export const authGuard: CanActivateFn = () => {
   if(authService.isLoggedIn()){
     return true;
   }
-   router.parseUrl('/login');
-   return false;
+   return router.parseUrl('/login');
+ 
   
 };
